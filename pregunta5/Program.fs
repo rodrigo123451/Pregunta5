c5c616fb15ccb30b17bcb627df2b539c73f068a0
rodrigo123451 <85976210+rodrigo123451@users.noreply.github.com>
2021-09-22 22:17:46 -0400

    Add files via upload

---
+++ pregunta5/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "hola mundo F#!"
+    let lista=[1..10 ]
+    printfn"%A"lista
+    printfn"pares"
+    let lista1=[for i in 1..10 do if i%2=0 then yield i]
+    printfn"%A"lista1
+    printfn"impares"
+    let lista2=[for i in 1..10 do if i%2=1 then yield i]
+    printf"%A"lista2
+
+
+    
+    
+  
+    
+
+
+    Console.ReadKey() |>ignore
+    0 // return an integer exit code
+   
